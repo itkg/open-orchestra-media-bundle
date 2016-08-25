e611a8b1435ad5123e89bffd18ecc4926c99fb53
itkg-ngilain <noel.gilain@businessdecision.com>
2016-08-25 15:06:51 +0200

    MediaRepository use UseTrackableTrait

--- MediaModelBundle/Repository/MediaRepository.php
+++ MediaModelBundle/Repository/MediaRepository.php
@@ -8,14 +8,16 @@ use OpenOrchestra\Media\Model\MediaInterface;
 use OpenOrchestra\Media\Repository\MediaRepositoryInterface;
 use OpenOrchestra\ModelBundle\Repository\RepositoryTrait\KeywordableTrait;
 use OpenOrchestra\ModelInterface\Repository\RepositoryTrait\KeywordableTraitInterface;
-use OpenOrchestra\ModelInterface\Model\UseTrackableInterface;
+use OpenOrchestra\ModelInterface\Repository\RepositoryTrait\UseTrackableTraitInterface;
+use OpenOrchestra\ModelBundle\Repository\RepositoryTrait\UseTrackableTrait;
 
 /**
  * Class MediaRepository
  */
-class MediaRepository extends DocumentRepository implements MediaRepositoryInterface, KeywordableTraitInterface
+class MediaRepository extends DocumentRepository implements MediaRepositoryInterface, KeywordableTraitInterface, UseTrackableTraitInterface
 {
     use KeywordableTrait;
+    use UseTrackableTrait;
 
     /**
      * @param string $folderId
@@ -68,33 +70,5 @@ class MediaRepository extends DocumentRepository implements MediaRepositoryInter
     public function findOneByName($name)
     {
         return $this->findOneBy(array('name' => $name));
-    }
-
-    /**
-     * @param string $nodeId
-     *
-     * @return array
-     */
-    public function findUsedInNode($nodeId)
-    {
-        $qb = $this->createQueryBuilder();
-
-        $qb->field('useReferences.' . UseTrackableInterface::KEY_NODE . '.' . $nodeId)->exists('true');
-
-        return $qb->getQuery()->execute();
-    }
-
-    /**
-     * @param string $contentId
-     *
-     * @return array
-     */
-    public function findUsedInContent($contentId)
-    {
-        $qb = $this->createQueryBuilder();
-
-        $qb->field('useReferences.' . UseTrackableInterface::KEY_CONTENT . '.' . $contentId)->exists('true');
-
-        return $qb->getQuery()->execute();
     }
 }
