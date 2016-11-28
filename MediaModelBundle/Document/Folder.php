f6d51a7cd977e70fd087c3677c6267eadc10474b
itkg-ngilain <noel.gilain@businessdecision.com>
2016-11-28 17:03:46 +0100

    Remove Folder:getPath redeclaration

--- MediaModelBundle/Document/Folder.php
+++ MediaModelBundle/Document/Folder.php
@@ -179,12 +179,4 @@ abstract class Folder implements FolderInterface
     {
         $this->siteId = $siteId;
     }
-
-    /**
-     * @return string
-     */
-    public function getPath()
-    {
-        return '';
-    }
 }
