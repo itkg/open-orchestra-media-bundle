3f5f2c9347fdcad0bf66539951cad71ccf841e32
itkg-nanne <nicolas.anne@businessdecision.com>
2017-05-23 10:32:55 +0200

    refresh test

--- MediaModelBundle/Tests/Form/Type/Component/FolderChoiceTypeTest.php
+++ MediaModelBundle/Tests/Form/Type/Component/FolderChoiceTypeTest.php
@@ -2,9 +2,11 @@
 
 namespace OpenOrchestra\MediaModelBundle\Tests\Form\Type\Component;
 
+use Doctrine\Common\Collections\ArrayCollection;
 use OpenOrchestra\BaseBundle\Tests\AbstractTest\AbstractBaseTestCase;
 use Phake;
 use OpenOrchestra\MediaModelBundle\Form\Type\Component\FolderChoiceType;
+use Symfony\Component\Form\ChoiceList\View\ChoiceView;
 
 /**
  * Class FolderChoiceTypeTest
@@ -76,5 +78,48 @@ class FolderChoiceTypeTest extends AbstractBaseTestCase
                 )
             )
         );
+    }
+
+    /**
+     * Test buildView
+     */
+    public function testBuildView()
+    {
+        $folder0 = Phake::mock($this->folderClass);
+        $folder0Id = 'folder0Id';
+        $folder0Name = 'folder0Name';
+        Phake::when($folder0)->getId()->thenReturn($folder0Id);
+        Phake::when($folder0)->getName(Phake::anyParameters())->thenReturn($folder0Name);
+
+        $folder1 = Phake::mock($this->folderClass);
+        $folder1Id = 'folder1Id';
+        $folder1Name = 'folder1Name';
+        Phake::when($folder1)->getId()->thenReturn($folder1Id);
+        Phake::when($folder1)->getName(Phake::anyParameters())->thenReturn($folder1Name);
+
+        Phake::when($folder0)->getSubFolders()->thenReturn(new ArrayCollection(array($folder1)));
+        Phake::when($folder1)->getSubFolders()->thenReturn(new ArrayCollection());
+
+        $choices = array(
+            new ChoiceView($folder0, $folder0Id, $folder0Name, array()),
+            new ChoiceView($folder1, $folder1Id, $folder1Name, array()),
+        );
+
+        $formView = Phake::mock('Symfony\Component\Form\FormView');
+        $formInterface = Phake::mock('Symfony\Component\Form\FormInterface');
+
+        $formView->vars['choices'] = $choices;
+
+        $this->form->buildView($formView, $formInterface, array());
+
+        $this->assertSame(array(
+            'data-depth' => 0,
+            'data-last' => false,
+        ), $formView->vars['choices']['folder0Id']->attr);
+        $this->assertSame(array(
+            'data-depth' => 1,
+            'data-last' => true,
+        ), $formView->vars['choices']['folder1Id']->attr);
+
     }
 }
